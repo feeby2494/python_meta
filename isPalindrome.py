a6ed488791ae85d79dd9a1088cca990e6dfc5fd7
feeby2494 <toby2494@hotmail.com>
2022-12-04 22:00:05 -0600

    For isPalindrome.py:
    Added a main loop with option to quit by typing 'Q'.
    Added user input, so user can type words to test.
    Added infinite loop, until user decides to quit.

--- isPalindrome.py
+++ isPalindrome.py
@@ -12,5 +12,12 @@ def isPalindrome(string):
 		endIndex -= 1		
 	return True
 
-
-print(isPalindrome('racecar'))
+if __name__ == "__main__":
+	end = False
+	while end is False:
+		string = input("Give me a string to check if it's spelled the same backwards:\nType 'Q' to quit\n")
+		if string.lower() == 'q':
+			end = True
+			break; 
+		palindrome = isPalindrome(string)
+		print("True, it's a palindrome" if palindrome is True else "False, it's not a palindrome")
