7da37bfe3280da3ceb88e61b2c64a8c2c01e1e39
feeby2494 <toby2494@hotmail.com>
2022-12-04 20:47:49 -0600

    Added two Meta cert exercises: one for error exceptions and file ops.

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+def divide_by(a, b):
+    return a / b
+    
+try:
+    ans = divide_by( 40  )
+# except Exception as e:
+#     print("Something went wrong!", e)
+#     print(e.__class__)
+except ZeroDivisionError as e:
+    print(e, "We cannot divide by zero.")
+except Exception as e:
+    print(e, "Something went wrong.")
